232ddbae6c3fd2bf39be562d8b4ee37b2e8df378
Anna Kazmirchuk <93005766+AnnaKazmirchuk@users.noreply.github.com>
2022-09-26 20:47:05 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,16 +1,20 @@
 const links = Array.from(document.querySelectorAll('.menu__link'));
 
-for (let link of links) {
+for (let link of link) {
     link.onclick = () => {
+
         const parent = link.parentElement;
-        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active'
-        } else {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub'
+        
+        if (parent.querySelector('.menu_sub') !== null) {
+            if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
+                parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
+            } else {
+                parent.querySelector('.menu_sub').className = 'menu menu_sub';
+            }
         }
 
-        if (link.closest('.menu_main')) {
-            return false
+        if (link.closest('.menu_main') && parent.querySelector('.menu_sub')) {
+            return false;
         }
     }
 }
